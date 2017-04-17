bf1d2623fe051e41d3285f3f7b572130adb0b85e
Vinícius S. Diaz <diaz@alunos.utfpr.edu.br>
2017-04-16 21:51:02 -0300

    Código de Leitura do Sensor de Ultrassom- revisao

---
+++ Pinagem.h
@@ -0,0 +1,22 @@
+#ifndef PINAGEM_H
+#define PINAGEM_H
+
+
+//Pinos do Ultrassom
+const int TRIGERPIN_01 = 4;//Gpio da Biblioteca wiringiPi
+const int ECHOPIN_01 = 5; 
+
+
+
+
+//--CONSTANTES DE SISTEMA
+
+ // Velocidade do som 340,29 m/s -&gt; 34029 cm/s
+ #define   VELOCIDADE_DO_SOM  34029
+
+
+
+
+#endif //-- Definicoes
+
+
---
+++ Ultrassom.h
@@ -0,0 +1,44 @@
+#ifndef _ULTRASSOM_H_
+#define _ULTRASSOM_H_
+
+#include <wiringPi.h>
+#include <sys/time.h>
+#include"Pinagem.h"
+
+
+class Ultrassom{
+    
+private:
+    int _triggerPin;
+    int _echoPin;
+    
+    volatile long startTimeUsec;
+    volatile long endTimeUsec;
+    double distanceMeters;
+    long travelTimeUsec;
+    long now;
+    
+    //Funcões Privadas
+    void setTriggerPin(const int triggerIn);//--Seta pino do trigger
+    void setEchoPin(const int echoIn);//--Seta pino do echo
+    void recordPulseLength();
+    
+    
+public:
+    
+    Ultrassom(){};
+    
+    ~Ultrassom(){};
+    
+    
+    
+    float calculaDistancia(int timeout);//--retorna o valor medido pela classe
+    void iniciaUltrassom(const int triggerIn, const int echoIn);
+    
+    
+};
+
+
+#endif
+
+
