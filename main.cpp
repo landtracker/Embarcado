378ed659aebcd304a8ab4df370849486fecc7fd0
Vinícius S. Diaz <diaz@alunos.utfpr.edu.br>
2017-04-17 07:54:43 -0300

    Edição da main - algumas funções novas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Ultrassom.h"
 #include"Pinagem.h"
 #include <iostream>
+#include<string>
 #include <wiringPi.h>
 
 using namespace std;
@@ -8,16 +9,32 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-   wiringPiSetup();
    
-    Ultrassom ultra1;
-    ultra1.iniciaUltrassom(TRIGERPIN_01, ECHOPIN_01);
+    bool execute = true;
+    string cancela;
     
-    while(1)
+    wiringPiSetup();
+   
+    Ultrassom ultra1;//Objeto do Ultrassom
+    ultra1.iniciaUltrassom(TRIGERPIN_01, ECHOPIN_01);//Pinos trigger e echo setados na Pinagem.h
+    
+    while(execute)
     {
-        cout<<" Saida do Ulltrasom em cm: "<<ultra1.calculaDistancia(30000)<<endl;    
+        cout<<" Saida do Ulltrasom em cm: "<<ultra1.calculaDistancia(30000)<<endl; //Medindo a distancia do ulytrassom 
         delay(300);
+        
+        
+        cin >> cancela;    
+        if(cancela == "c")
+        {
+            execute = false;
+            break;
+        }
+        
     }
     
+    cout<<"Encerrando o programa"<<endl;
+    
+   
     return 0;
 }
