ac84aecde56287de820095619c32c7adcbc0f684
Trenthor <ltrenthor@gmail.com>
2017-04-23 23:07:37 -0300

    Adicionado sudo ao comando de compile

--- compila.sh
+++ compila.sh
@@ -3,4 +3,4 @@
 #Link pro softpwm https://projects.drogon.net/raspberry-pi/wiringpi/software-pwm-library/
 
 g++ main.cpp -o test -lwiringPi MotorDC.cpp  Ultrassom.cpp Servo.cpp
-./test
+sudo ./test
