fa0954d8667d2282eb119258a7462c67c013a8b8
Vinícius S. Diaz <diaz@alunos.utfpr.edu.br>
2017-06-15 09:57:07 -0300

    Servos Atualizados para comunicação i2c com Arduino

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
 g++ -W -o landTrackerEmbarcado ClietTCP.cpp Comando.cpp Constantes_\&_Pinos.h defines.h ExecutorDeComandos.cpp main.cpp MotorDC.cpp ServerTCP.cpp Servo.cpp Ultrassom.cpp -lwiringPi -std=c++11 -lpthread
+
