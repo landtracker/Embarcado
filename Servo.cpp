d6ef7269c225edd7433ba53e245f0f1b7b9b2bf9
Vinícius S. Diaz <diaz@alunos.utfpr.edu.br>
2017-06-15 15:00:27 -0300

    Servo atualizado

---
+++ Servo.cpp
@@ -0,0 +1,147 @@
+#include "Servo.h"
+#include"Constantes_&_Pinos.h"
+
+
+Servo::Servo()
+{
+    servoPin = -1;
+    pwm = 0;
+    range = 0;
+    flagArduino = false;
+    
+}
+
+Servo::~Servo()
+{
+    servoPin = -1;
+    pwm = 0;
+    range = 0;
+    
+}
+
+void Servo::iniciaServo(const int _servoIn)
+{
+    
+    setServoPin(_servoIn);
+    pinMode(servoPin, PWM_OUTPUT);
+    pwmSetMode (PWM_MODE_MS);
+    pwmSetClock (PWMCLOCK);
+    pwmSetRange (PWMRANGE);
+    Alinhar();    
+    
+}
+
+void Servo::setServoPin(const int _servoIn)
+{
+    
+    servoPin = _servoIn;
+}
+
+void Servo::setAngulo(const unsigned _angulo)
+{
+    
+    
+    
+    if(!getFlag())
+    {
+        pwmWrite(servoPin, ((_angulo/1.8) + 100));
+    }
+    
+    else
+        
+    {
+        setAnguloServoArduino(_angulo);
+    }
+    
+}
+
+
+void Servo::varreduraD()
+{
+    
+    for(int i = 0; i <= 18; i++)
+    {
+        setAngulo(10*i);
+        delay(150);
+    }
+    
+    
+}
+
+void Servo::varreduraE()
+{
+    
+    for(int i = 18; i >= 0; i--)
+    {
+        setAngulo(10*i);
+        delay(150);
+    }
+    
+    
+    
+}
+
+void Servo::Alinhar()
+{
+    setAngulo(90);
+    
+}
+
+void Servo::iniciaServoArduino(const int _servoIn)
+{
+    
+    if( _servoIn >0 &  _servoIn <4)
+    {
+        servoArduino = _servoIn;
+        setFlagArduino(true);
+    }
+    
+    else
+    {
+        servoArduino = -1;
+        setFlagArduino(false);
+        cout<<"Servo Arduino Invalido"<<endl;
+    }
+    
+}
+
+void Servo::setAnguloServoArduino(const unsigned _angulo)
+{
+    unsigned fd = 0; 
+    fd = wiringPiI2CSetup(i2cAddress);
+    
+    if( servoArduino > 0 & servoArduino < 4)
+    {
+        wiringPiI2CWrite(fd, servoArduino );
+        delay(30);
+        
+        if(_angulo >=0 & _angulo <=180)
+        {
+            wiringPiI2CWrite(fd, _angulo);   
+            
+        }
+        
+        else
+        {
+            cout<<"Angulo Invalido no Servo Arduino!"<<endl;
+        }
+        
+    }
+    
+    else
+    {
+        cout<<"Servo Arduino Invalido"<<endl;   
+    }        
+    
+}
+
+
+void Servo::setFlagArduino(const bool _flag)
+{
+    flagArduino = _flag;
+}
+
+const bool Servo:: getFlag() const
+{
+    return flagArduino;
+}
---
+++ Servo.h
@@ -0,0 +1,51 @@
+#ifndef _SERVO_H_
+#define _SERVO_H_
+
+#include <iostream>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+
+using namespace std;
+const unsigned i2cAddress = 0x04;
+
+
+
+
+class Servo{
+    
+private:
+    
+    int servoPin;
+    unsigned pwm;
+    unsigned range;
+    unsigned angulo;
+    unsigned servoArduino;
+    bool flagArduino;
+    
+    void setServoPin(const int _servoIn);//--Seta pino de controle
+    void setAnguloServoArduino(const unsigned _angulo);
+
+    
+    
+    
+public:
+    
+    
+    Servo();
+    ~Servo();
+    
+    void iniciaServo(const int _servoIn);
+    void iniciaServoArduino(const int _servoIn);
+    
+    void setAngulo(const unsigned _angulo);
+    void Alinhar();
+    void varreduraD();
+    void varreduraE();
+    void setFlagArduino(const bool _flag); 
+    const bool getFlag() const;
+};
+
+
+#endif
